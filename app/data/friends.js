1d2cb550d855d038e5a3a9d03b347c2fd79a1f8a
snavdeepsingh <snavdeepsingh@gmail.com>
2018-04-24 15:05:24 -0500

    added friends.js data

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Josh",
+    "photo" : "https://goo.gl/images/n7cpHW",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Sherri",
+    "photo" : "https://goo.gl/images/xWdMr3",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Kirill",
+    "photo" : "https://goo.gl/images/ED9SBS",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Amy",
+    "photo" : "https://goo.gl/images/Rv7rfK",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Gavin",
+    "photo" : "https://goo.gl/images/7rXsnz",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kayla",
+    "photo" : "https://goo.gl/images/29Kv4g",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "PJ",
+    "photo" : "https://goo.gl/images/AYDJw3",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Janie",
+    "photo" : "https://goo.gl/images/eEDgD9",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Andre",
+    "photo" : "https://goo.gl/images/t46ygq",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Po",
+    "photo" : "https://goo.gl/images/2zAvTT",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Gru",
+    "photo" : "https://goo.gl/images/Kdb8cV",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Shifu",
+    "photo" : "https://goo.gl/images/KGhGX8",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Stuart",
+    "photo" : "https://goo.gl/images/Z17b8B",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Stewie",
+    "photo" : "https://goo.gl/images/bG73q6",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
